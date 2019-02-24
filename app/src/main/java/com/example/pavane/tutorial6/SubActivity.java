401b5685c023d9a0f0b75e7ee8ff4935ad0a9773
Lee, Byungman <pavane90@gmail.com>
2019-02-24 16:13:04 +0900

    first

---
+++ app/src/main/java/com/example/pavane/tutorial6/SubActivity.java
@@ -0,0 +1,19 @@
+package com.example.pavane.tutorial6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        TextView nameText = (TextView) findViewById(R.id.nameText);
+        Intent intent = getIntent();
+        nameText.setText(intent.getStringExtra("nameText").toString());
+    }
+}
